5a8c56823250af8f86d41afd336319b45b2ced67
xuanhaibaka <dauhai2005@gmail.com>
2024-03-29 21:01:29 +0700

    fix

---
+++ app/api/login/route.ts
@@ -0,0 +1,66 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import prisma from "../../lib/primasdb";
+import { IsignInSchema } from "@/app/lib/type";
+import { signInSchema } from "@/app/lib/validation";
+import { createPermisson } from "@/app/lib/actions/permisstion.action";
+
+// export const POST = async (request: Request) => {
+//   try {
+//     const body = await request.json();
+
+//     // const parseBody = signInSchema.safeParse(body);
+
+//     // if (!parseBody.success)
+//     //   return NextResponse.json(parseBody.error.message, { status: 422 });
+
+//     const user = await prisma.profile.findUnique({
+//       where: {
+//         email: body.email,
+//       },
+//       include: {
+//         role: true,
+//       },
+//     });
+
+//     if (!user)
+//       return NextResponse.json(
+//         { message: "account not exists" },
+//         { status: 400 }
+//       );
+
+//     const passwordMatch = await bcrypt.compare(body.password, user.password);
+//     if (!passwordMatch) {
+//       return NextResponse.json(
+//         { message: "Invalid email or password" },
+//         { status: 400 }
+//       );
+//     }
+
+//     return NextResponse.json({
+//       profile: {
+//         id: user.id,
+//         fullName: user.fullName,
+//         email: user.email,
+//         genders: user.genders,
+//         role: {
+//           id: user.role?.id,
+//           role: user.role?.roleName,
+//         },
+//       },
+//     });
+//   } catch (error) {
+//     console.error("Error retrieving user:", error);
+//     throw error;
+//   }
+// };
+export const POST = async (request: Request) => {
+  const data = await request.json();
+  const product = await createPermisson(data);
+
+  if (!product) {
+    return NextResponse.json({ message: "ERROR FROM SERVER" }, { status: 500 });
+  }
+
+  return NextResponse.json(product);
+};
