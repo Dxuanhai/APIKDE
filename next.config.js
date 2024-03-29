70ea6f14edecee037f942e4834afb015e590c475
xuanhaibaka <dauhai2005@gmail.com>
2024-03-29 18:35:18 +0700

    x

--- next.config.js
+++ next.config.js
@@ -1,36 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   async headers() {
     return [
       {
-        source: "/api/:path*",
-        destination: "https://apikde.vercel.app/api/:path*",
-
+        source: "/api/*",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
           },
           {
             key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+            value: "Content-Type, Authorization",
           },
-        ],
-      },
-      {
-        source: "/api/auth/:path*",
-        destination: "https://apikde.vercel.app/api/auth/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,POST" },
-          { key: "Access-Control-Allow-Headers", value: "*" },
         ],
       },
     ];
   },
 };
-
-module.exports = nextConfig;
