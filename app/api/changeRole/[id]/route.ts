3855dcd8f2b05b037f19771d45fc7af9e1df8296
xuanhaibaka <dauhai2005@gmail.com>
2024-04-18 14:57:22 +0700

    changeRole

---
+++ app/api/changeRole/[id]/route.ts
@@ -0,0 +1,105 @@
+import { z } from "zod";
+import prisma from "../../../lib/primasdb";
+import { NextResponse } from "next/server";
+
+interface IParams {
+  id?: string;
+}
+
+export const GET = async (
+  request: Request,
+  { params }: { params: IParams }
+) => {
+  const { id } = params;
+
+  if (!id)
+    return NextResponse.json(
+      { message: "Not found parametes" },
+      { status: 422 }
+    );
+  const parsedId = parseInt(id, 10);
+  if (typeof parsedId !== "number")
+    return NextResponse.json({ message: "Invalid parametes" }, { status: 422 });
+
+  const profile = await prisma.profile.findUnique({
+    where: { id: parsedId },
+    include: {
+      role: true,
+    },
+  });
+
+  if (!profile) {
+    return new NextResponse(JSON.stringify({ message: "ERROR FROM SERVER " }), {
+      status: 500,
+    });
+  }
+  return NextResponse.json(
+    {
+      profile: {
+        id: profile.id,
+        fullName: profile.fullName,
+        email: profile.email,
+        genders: profile.genders,
+        role: {
+          id: profile.role?.id,
+          role: profile.role?.roleName,
+        },
+      },
+    },
+    { status: 200 }
+  );
+};
+
+export const PUT = async (
+  request: Request,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    const { id } = params;
+    const data = await request.json();
+
+    const parsedId = parseInt(id, 10);
+    if (typeof parsedId !== "number")
+      return NextResponse.json(
+        { message: "Invalid parametes" },
+        { status: 422 }
+      );
+    const profile = await prisma.profile.findUnique({
+      where: { id: parsedId },
+      include: {
+        role: true,
+      },
+    });
+
+    if (!profile) {
+      return NextResponse.json(
+        { message: "Profile not found" },
+        { status: 404 }
+      );
+    }
+
+    const newProfile = await prisma.profile.update({
+      where: {
+        id: profile.id,
+      },
+      data: {
+        roleId: data.roleId,
+      },
+    });
+
+    return NextResponse.json(newProfile);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        { message: "Invalid parameters" },
+        { status: 422 }
+      );
+    }
+    console.log(error);
+
+    return NextResponse.json(
+      { message: "Error updating profile" },
+      { status: 500 }
+    );
+  }
+};
---
+++ app/api/changeRole/route.ts
@@ -0,0 +1,41 @@
+import {
+  fetchProfiles,
+  fetchProfilesLimit,
+} from "@/app/lib/actions/profiles.action";
+
+import { Tprofile } from "@/app/lib/type";
+
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (request: NextRequest) => {
+  const { searchParams } = new URL(request.url);
+  const limit = searchParams.get("take");
+  const skip = searchParams.get("skip");
+
+  let products: Tprofile[];
+  if (!limit || !skip) {
+    products = await fetchProfiles();
+  } else {
+    const parsedLimit = parseInt(limit, 10);
+    const parsedSkip = parseInt(skip, 10);
+    if (typeof parsedLimit !== "number") {
+      return NextResponse.json(
+        { message: "Invalid parametes" },
+        { status: 422 }
+      );
+    }
+    if (typeof parsedSkip !== "number") {
+      return NextResponse.json(
+        { message: "Invalid parametes" },
+        { status: 422 }
+      );
+    }
+    products = await fetchProfilesLimit(parsedSkip, parsedLimit);
+  }
+
+  if (!products) {
+    return NextResponse.json({ message: "ERROR FROM SERVER" }, { status: 500 });
+  }
+
+  return NextResponse.json(products);
+};
